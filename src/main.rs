2d2772c93331b0a3beb6d645feff5a25086731f7
darckfast <21337578+Darckfast@users.noreply.github.com>
2026-06-09 14:57:17 -0300

    first commit

---
+++ src/main.rs
@@ -0,0 +1,27 @@
+use fastly::http::StatusCode;
+use fastly::{Error, Request, Response};
+use serde_json::json;
+use std::time::{SystemTime, UNIX_EPOCH};
+
+use serde::Serialize;
+
+#[derive(Serialize)]
+struct Payload {
+    time: u128,
+}
+
+#[fastly::main]
+fn main(_req: Request) -> Result<Response, Error> {
+    let payload = Payload {
+        time: SystemTime::now()
+            .duration_since(UNIX_EPOCH)
+            .unwrap()
+            .as_millis(),
+    };
+
+    let mut resp = Response::new();
+
+    resp.set_body_json(&payload)?;
+
+    Ok(resp)
+}
